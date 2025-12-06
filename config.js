5bbeb82e2a3d3704f774beba083992c6e7d77a53
jhanfong150983-dot <jhanfong150983@gmail.com>
2025-12-06 09:00:13 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ const APP_CONFIG = {
    * 3. 部署 → 管理部署作業
    * 4. 複製網址貼到這裡
    */
-  API_URL: 'https://script.google.com/macros/s/AKfycbxgskXfK1AM1hQCj0bHCP1MulQq7Ic2KrueaG3zAZtLI_DZ4HgPvqqHdPNCPbsYKzUgNg/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwngAD79VvvM2znovJeclK6mQlYk2cwF4v3xp7Ffuq8BxwkZxqEgm3AzMgjoAZIV-ghxw/exec',
   
   
   // ==========================================
@@ -190,7 +190,6 @@ if (!APP_CONFIG.isDevelopment()) {
   Object.freeze(APP_CONFIG);
 
 }
-
 
 
 
