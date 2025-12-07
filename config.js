c9ba6d6ad9016b32fc8f59e5e89340f2e69cd9fc
jhanfong150983-dot <jhanfong150983@gmail.com>
2025-12-08 07:19:40 +0800

    Teacher2-2

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ const APP_CONFIG = {
    * 3. 部署 → 管理部署作業
    * 4. 複製網址貼到這裡
    */
-  API_URL: 'https://script.google.com/macros/s/AKfycbzd3BiOrz0jLR_pGj792KdvXWjmmtb4fwAETDQDLdTOjj9rKijI7MiZcHSnmUnHXQmzlA/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwb8aXAT9natmwZAngqrzmHOXHpXSxJ352t4S5bh7ZLFm7KfDeFPtkFtHg5FEHRwbR-8g/exec',
   
   
   // ==========================================
@@ -190,6 +190,7 @@ if (!APP_CONFIG.isDevelopment()) {
   Object.freeze(APP_CONFIG);
 
 }
+
 
 
 
