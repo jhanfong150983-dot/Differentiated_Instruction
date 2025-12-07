a1168d73dd98aeff7b724b114ee48d912d4597ee
jhanfong150983-dot <jhanfong150983@gmail.com>
2025-12-08 04:04:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ const APP_CONFIG = {
    * 3. 部署 → 管理部署作業
    * 4. 複製網址貼到這裡
    */
-  API_URL: 'https://script.google.com/macros/s/AKfycbwpJPeAH7YTjsUKurjLAXIwgkLvAJVR8jtMz18Bx3h77fZQ7LpDMOCpaxSWVskGx4CdyQ/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyFto1_5KcFJGoqPL5Ikq22LchBbRFkXNrjthoCtoMoCxPX0E-nDVx_cLdl9wbm_9yOKg/exec',
   
   
   // ==========================================
@@ -190,6 +190,7 @@ if (!APP_CONFIG.isDevelopment()) {
   Object.freeze(APP_CONFIG);
 
 }
+
 
 
 
